bf40c1b26309d6363d345213c3ccf76d090f8788
angelasinuosun <sinuos2@uw.edu>
2024-05-26 16:34:44 -0700

    Update labels

--- Time_Trend_Chart.R
+++ Time_Trend_Chart.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 data <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-jail-rates-1990.csv")
-County_data <- data %>%
+county_data <- data %>%
   filter(state == "WA" & county_name == "King County") %>%
   pivot_longer(cols = c(aapi_prison_pop_rate, black_prison_pop_rate, native_prison_pop_rate, white_prison_pop_rate), 
                names_to = "race", 
@@ -13,11 +13,11 @@ County_data <- data %>%
                        latinx_prison_pop_rate = "Latinx",
                        native_prison_pop_rate = "Native American",
                        white_prison_pop_rate = "White"))
-ggplot(County_data, aes(x = year, y = prison_pop_rate, color = race)) +
+ggplot(county_data, aes(x = year, y = prison_pop_rate, color = race)) +
   geom_line() +
-  scale_x_continuous(breaks = seq(min(County_data$year), max(County_data$year), by = 5)) +
-  labs(title = "Prison Population Rates in King County, WA",
+  scale_x_continuous(breaks = seq(min(county_data$year), max(county_data$year), by = 5)) +
+  labs(title = "Prison Population Rate in King County, WA",
        x = "Year",
-       y = "Prison Population Rate",
+       y = "Prison Population per 100,000 People",
        color = "Race/Ethnicity") +
   theme_minimal()
