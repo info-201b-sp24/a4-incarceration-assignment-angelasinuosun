211e5a4f36d107ea5cf38cae0d5dbd3cbefafde1
angelasinuosun <sinuos2@uw.edu>
2024-05-26 17:35:18 -0700

    Update Labels

--- Map.R
+++ Map.R
@@ -1,4 +1,3 @@
-# WTS: 2018 U.S. Prison Rate Distribution Map
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
@@ -7,11 +6,6 @@ library(scales)
 
 rate_data <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-jail-rates-1990.csv")
 pop_data <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-jail-pop.csv")
-
-# The new pop_data is not previouos data2: careful in dataset in rmd
-
-# Prison rate are all NAs (natural)
-# Use jail, change chart 1 to prison
 
 highest_jail_race <- function(x){
   races <- c("aapi", "black", "latinx", "native", "white")
@@ -28,8 +22,6 @@ rate_data <- rate_data %>%
          native_jail_pop_rate, white_jail_pop_rate), 
     ~ highest_jail_race(c(...))))
 
-# The race has the most frequently highest jail population rate among 
-# all counties in each state
 most_frequent_race <- rate_data %>%
   filter(year == 2018) %>%
   count(state, race_highest_jail) %>%
@@ -57,18 +49,27 @@ state_jail_data <- state_jail_data %>%
 state_shape <- state_shape %>%
   left_join(state_jail_data, by = "region")
 
-# Normalize total_jail_pop for intensity
 state_shape$total_jail_pop_norm <- rescale(state_shape$total_jail_pop, 
                                            to = c(0.5, 1), na.rm = TRUE)
-
-# Plot the map
+race_names <- c("aapi" = "Asian American / Pacific Islander",
+                "black" = "Black",
+                "latinx" = "Latinx",
+                "native" = "Native American",
+                "white" = "White")
 ggplot(state_shape, aes(x = long, y = lat, group = group)) +
   geom_polygon(aes(fill = race_highest_frequent, alpha = total_jail_pop_norm), color = "black") +
   scale_alpha(range = c(0.1, 1), na.value = 0.5) +
   coord_fixed(1.3) +
-  labs(fill = "Race (Most Frequent)", alpha = "Jail Population Intensity") +
+  labs(title = "2018 US Jail Population", 
+       subtitle = "An Analysis of Intensity and Race", 
+       fill = "Most Frequent Race/Ethnicity with the Highest Jail Population Rate", 
+       alpha = "Total Jail Population Intensity") +
+  scale_fill_discrete(labels = race_names) +
+  theme_minimal() +
   theme_void() +
-  theme(legend.position = "bottom") +
-  theme_minimal()
-# ADD x y labels and title(2018xx)
-# Modify legends
+  theme(legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    legend.box = "vertical") +
+  guides(fill = guide_legend(order = 1, title.position = "top", title.hjust = 0.5),
+    alpha = guide_legend(order = 0, position = "right"))
